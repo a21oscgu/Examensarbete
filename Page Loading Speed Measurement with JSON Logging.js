453beeb4da7e7e936cdee06ddca1cd74185b1059
a21oscgu <a21oscgu@student.his.se>
2024-02-22 21:51:34 +0100

    Created empty tampermonkey script
    For measuring page loading speed on the website

---
+++ Page Loading Speed Measurement with JSON Logging.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-22
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
