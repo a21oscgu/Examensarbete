5ee96fe0abb96c52c47fbc7e4e1de6cb2345cf13
a21oscgu <a21oscgu@student.his.se>
2024-02-24 14:39:30 +0100

    Added code to generate random author (JSON)

--- TampermonkeyScripts/GenerateRandomizedDataScriptForJSON.js
+++ TampermonkeyScripts/GenerateRandomizedDataScriptForJSON.js
@@ -30,11 +30,18 @@
         /*Code to generate data in form of JSON*/
         var textArea = document.getElementById("output");
         var headline = generate_sentence();
-        var author = generate_sentence();
+
+        let firstNames = ["John", "Emma", "Michael", "Sophia", "James", "Olivia", "William", "Ava", "Alexander", "Isabella", "Ethan", "Mia", "Benjamin", "Charlotte", "Daniel", "Amelia", "Jacob", "Harper", "Matthew", "Evelyn", "Lucas", "Abigail", "Jackson", "Emily", "David", "Madison", "Joseph", "Lily", "Logan", "Grace", "Samuel", "Chloe", "Henry", "Avery", "Owen", "Ella", "Sebastian", "Sofia", "Gabriel", "Scarlett", "Carter", "Victoria", "Jayden", "Riley", "Dylan", "Claire", "Luke", "Nora"];
+        let lastNames = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson", "Clark", "Lewis", "Lee", "Walker", "Hall", "Allen", "Young", "King", "Wright", "Scott", "Adams", "Green", "Evans", "Baker", "Hill", "Morris", "Ward", "Cox", "Rivera", "Cook", "Bailey", "Bell", "Murphy"];
+        let randomFirstNameIndex = Math.floor(Math.random() * firstNames.length);
+        let randomLastNameIndex = Math.floor(Math.random() * lastNames.length);
+        let randomFirstName = firstNames[randomFirstNameIndex];
+        let randomLastName = lastNames[randomLastNameIndex];
+        let author = randomFirstName + " " + randomLastName;
 
         let day = Math.floor(Math.random()*31) + 1;
         let month = Math.floor(Math.random() * 12) + 1;
-        const year = Math.floor(Math.random() * (2025 - 2000)) + 2000;
+        let year = Math.floor(Math.random() * (2025 - 2000)) + 2000;
         var date = day + "-" + month + "-" + year;
 
         var content = generate_sentence();
