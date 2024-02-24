aee55385b80f9d1c021a01e0fe34bf4355b92ae7
a21oscgu <a21oscgu@student.his.se>
2024-02-24 12:19:47 +0100

    Create GenerateRandomizedDataScript.js

---
+++ GenerateRandomizedDataScript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         GenerateRandomizedDataScript
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-24
+// @description  Generate Randomized Data
+// @author       You
+// @match        https://oscarswebsite.se/Examensarbete/GenerateRandomizedData/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
