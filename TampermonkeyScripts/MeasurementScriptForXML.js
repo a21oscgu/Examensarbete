b0f41d0e5303775901f75a0e2b9f29dd866a54a6
a21oscgu <a21oscgu@student.his.se>
2024-03-01 13:59:17 +0100

    Updated versions

--- TampermonkeyScripts/MeasurementScriptForXML.js
+++ TampermonkeyScripts/MeasurementScriptForXML.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MeasurementScript for XML
 // @namespace    http://tampermonkey.net/
-// @version      2024-02-25
+// @version      2024-03-01
 // @description  Page Loading Speed Measurement Script with JSON Logging
 // @author       You
 // @match        https://oscarswebsite.se/Examensarbete/XMLApplication/
